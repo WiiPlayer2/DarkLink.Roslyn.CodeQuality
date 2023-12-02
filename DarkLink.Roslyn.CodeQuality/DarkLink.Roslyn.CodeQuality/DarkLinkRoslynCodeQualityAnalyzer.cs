d34f95f2c01815749f1026898f51c718601d4e08
Waldemar Tomme <waldemar.tomme@bluehands.de>
2023-12-02 14:03:58 +0100

    More cleanup + rules

--- DarkLink.Roslyn.CodeQuality/DarkLink.Roslyn.CodeQuality/DarkLinkRoslynCodeQualityAnalyzer.cs
+++ DarkLink.Roslyn.CodeQuality/DarkLink.Roslyn.CodeQuality/DarkLinkRoslynCodeQualityAnalyzer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -9,34 +8,15 @@ namespace DarkLink.Roslyn.CodeQuality;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class DarkLinkRoslynCodeQualityAnalyzer : DiagnosticAnalyzer
 {
-    // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-    // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
-
-
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Diagnostics.Rule);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(
+        Diagnostics.CQ0001_MethodMixed,
+        Diagnostics.CQ0002_LogicInComposition,
+        Diagnostics.CQ0003_CompositionInLogic
+    );
 
     public override void Initialize(AnalysisContext context)
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
         context.EnableConcurrentExecution();
-
-        // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-        context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-    }
-
-    private static void AnalyzeSymbol(SymbolAnalysisContext context)
-    {
-        // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-        var namedTypeSymbol = (INamedTypeSymbol) context.Symbol;
-
-        // Find just those named type symbols with names containing lowercase letters.
-        if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-        {
-            // For all such symbols, produce a diagnostic.
-            var diagnostic = Diagnostic.Create(Diagnostics.Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-            context.ReportDiagnostic(diagnostic);
-        }
     }
 }
--- DarkLink.Roslyn.CodeQuality/DarkLink.Roslyn.CodeQuality/Diagnostics.cs
+++ DarkLink.Roslyn.CodeQuality/DarkLink.Roslyn.CodeQuality/Diagnostics.cs
@@ -5,23 +5,35 @@ namespace DarkLink.Roslyn.CodeQuality;
 
 public static class Diagnostics
 {
-    private const string Category = "Naming";
+    private const string Category = "CodeQuality";
 
-    public static readonly DiagnosticDescriptor Rule = CreateDescriptor(
-        "DarkLinkRoslynCodeQuality",
-        nameof(Resources.AnalyzerTitle),
-        nameof(Resources.AnalyzerMessageFormat),
-        nameof(Resources.AnalyzerDescription));
+    public static readonly DiagnosticDescriptor CQ0001_MethodMixed = CreateDescriptor(
+        "CQ0001",
+        nameof(Resources.CQ0001_Title),
+        nameof(Resources.CQ0001_MessageFormat),
+        nameof(Resources.CQ0001_Description));
+
+    public static readonly DiagnosticDescriptor CQ0002_LogicInComposition = CreateDescriptor(
+        "CQ0002",
+        nameof(Resources.CQ0002_Title),
+        nameof(Resources.CQ0002_MessageFormat),
+        nameof(Resources.CQ0002_Description));
+
+    public static readonly DiagnosticDescriptor CQ0003_CompositionInLogic = CreateDescriptor(
+        "CQ0003",
+        nameof(Resources.CQ0003_Title),
+        nameof(Resources.CQ0003_MessageFormat),
+        nameof(Resources.CQ0003_Description));
 
     private static DiagnosticDescriptor CreateDescriptor(
         string id,
         string titleResource,
-        string messageResource,
+        string messageTitleResource,
         string descriptionResource) =>
         new(
             id,
             CreateLocalizableString(titleResource),
-            CreateLocalizableString(messageResource),
+            CreateLocalizableString(messageTitleResource),
             Category,
             DiagnosticSeverity.Warning,
             true,
